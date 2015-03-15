41b0aab823e5e5e0284c15c443bdb6295df0181e
libbyschuknight <libbysk@gmail.com>
2015-03-16 12:23:21 +1300

    added image to overlay, adjusted css to postion

--- js/app.js
+++ js/app.js
@@ -5,8 +5,8 @@
 var $overlay = $('<div id = "overlay"></div>');
 var $image = $("<img>");
 
-// Add an image to the overlay
-// $overlay.append($image);
+// Add an image to the overlay - image code
+$overlay.append($image);
 
 // Add overlay - add overlay div into body html
 $("body").append($overlay);
@@ -16,7 +16,11 @@ $("body").append($overlay);
 //capture the click event on a link to an image
 $("#gallery a").click(function(event){
   event.preventDefault();
-  // var imageLocation = $(this).attr("href");
+  //get the image location - reference
+  var imageLocation = $(this).attr("href");
+
+    //update overlay with the image from the link
+    $image.attr("src", imageLocation);
 
     //Show the overlay
      $overlay.show();
