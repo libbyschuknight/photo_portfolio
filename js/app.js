1e0443cd2d53d8d4e52c43c2981ea79337849ac9
Libby Schumacher-Knight <libby@fluxfederation.com>
2019-06-13 14:59:37 +1200

    Add some ideas on using JS instead of jQuery

--- js/app.js
+++ js/app.js
@@ -7,40 +7,49 @@
 // change to being able to click through the photos when they pop up in lightbox type way
 
 var $overlay = $('<div id = "overlay"></div>');
-var $image = $("<img>");
-var $caption = $("<p></p>");
+// var overlay = document.getElementById('overlay');
+
+var $image = $('<img>');
+// var image = document.querySelectorAll('img');
+// document.querySelectorAll('img');
+
+var $caption = $('<p></p>');
 
 // Add an image to the overlay - image code
 $overlay.append($image);
 
+// $(parent).append(el);
+// parent.appendChild(el);
+// overlay.appendChild(image);
+
 // Add overlay - add overlay div into body html
-$("body").append($overlay);
+$('body').append($overlay);
 
 // Add a caption to overlay
 $overlay.append($caption);
 
-
-
 //capture the click event on a link to an image
-$("#gallery a").click(function(event){
+$('#gallery a').click(function(event) {
   event.preventDefault();
   //get the image location - reference
-  var imageLocation = $(this).attr("href");
+  var imageLocation = $(this).attr('href');
 
-    //update overlay with the image from the link
-    $image.attr("src", imageLocation);
+  //update overlay with the image from the link
+  $image.attr('src', imageLocation);
 
-    //Show the overlay
-     $overlay.show();
+  //Show the overlay
+  $overlay.show();
 
-     //Get child's alt atrribute and set caption
-     var captionText = $(this).children("img").attr("alt");
-     //update overlay with the caption from link - alt tag
-     $caption.text(captionText);
+  //Get child's alt atrribute and set caption
+  var captionText = $(this)
+    .children('img')
+    .attr('alt');
+  //update overlay with the caption from link - alt tag
+  $caption.text(captionText);
 });
 
 // When overlay is clicked
-$overlay.click(function(){
+$overlay.click(function() {
   //Hide the overlay (which will show whole page again)
   $overlay.hide();
-})
+});
